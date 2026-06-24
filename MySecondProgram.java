3278425314ab8a1288b22ef8adde480f7670ff33
jaydenkingston25-dot <jaydenkingston25@gmail.com>
2026-06-24 14:35:37 -0600

    Update MySecondProgram.java

--- MySecondProgram.java
+++ MySecondProgram.java
@@ -1,30 +1,27 @@
 public class MySecondProgram {
     
     public static void main(String[] args) {
-        
-        // TODO 1: Define two String variables (e.g., "Computer" and "Science", or "Cybersecurity" and "Analyst")
-        
-        
-        // TODO 2: Define an int variable and a double variable (e.g., 21 and 5.5)
-        
-        
-        // TODO 3: Define a 'char' variable called 'third'. 
-        // Use .charAt() to extract the 3rd character of your first string. (Careful with 0-based indexing!)
-        
-        
-        // TODO 4: Define a double variable called 'sum'. Add your two numbers together.
-        
-        
-        // TODO 5: Define a String called 'second'. 
-        // Rebuild your second string so that the 2nd character is uppercase!
-        
-        
-        // TODO 6: Define a String called 'password'. 
-        // Concatenate the pieces exactly as requested in the instructions.
-        
-        
-        
-        // Finally, print your generated password!
-        
+       String str1 = "Computer";
+        String str2 = "Science";
+
+        int num1 = 5;
+        double num2 = 3.5;
+
+        char third = str1.charAt(2);
+
+        double sum = num1 + num2;
+
+        String second = str2.substring(0, 1) + str2.substring(1, 2).toUpperCase() + str2.substring(2);
+
+        String password = "" + str2.charAt(0) + str2.charAt(1) + num1 + str1.charAt(str1.length() - 1) + num2 + "/";
+
+        System.out.println("1st String: " + str1);
+        System.out.println("2nd String: " + str2);
+        System.out.println("1st Number: " + num1);
+        System.out.println("2nd Number: " + num2);
+        System.out.println("Third Character from " + str1 + ": " + third);
+        System.out.println("Sum: " + sum);
+        System.out.println("Second: " + second);
+        System.out.println("Password: " + password);
     }
 }
